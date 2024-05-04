4de8b9851e34cd336139317b223461b8191d56e2
Brooke Chalmers <breq@breq.dev>
2024-05-04 01:14:37 -0400

    initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,12 +7,9 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    fontFamily: {
+      sans: ["var(--font-nunito-sans)"],
+      mono: ["var(--font-fira-code)"],
     },
   },
   plugins: [],
